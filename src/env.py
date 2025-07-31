1c948281567523f1d19ab2fd393cc816c14e9a6f
Cristian María Rodríguez Piastri <102370984+CrisRPia@users.noreply.github.com>
2025-07-31 20:07:58 -0300

    #6: Minimal working prototype (#26)
    Co-authored-by: Cristian Rodriguez <cristianrodriguez@Juan-Martin-Hernandez.local>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/env.py
+++ src/env.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel, Field
 
 
 class Environment(BaseModel, frozen=True):
-    google_cloud_api_key: str = Field(alias='GOOGLE_CLOUD_API_KEY')
+    google_cloud_api_key: str = Field(alias="GOOGLE_CLOUD_API_KEY")
 
 
 @cache
