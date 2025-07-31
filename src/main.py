8fce764f490643d6f374877009aaf06381b424f2
Cristian Rodriguez <cristianrodriguez@Juan-Martin-Hernandez.local>
2025-07-30 21:39:15 -0300

    we can now chat with gemini

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 from env import env
+from google import genai
 
 def main() -> None:
     print(f"{env() = }")
+
+    client = genai.Client(api_key=env().google_cloud_api_key)
+
+    chat = client.chats.create(model="gemini-2.0-flash")
+
+    for message in ["what the dog doin", "whos a good boi", "summarize what we have been talking about."]:
+        print(f"{message = }")
+        response = chat.send_message(message)
+        print(f"{(response.candidates or [])[0].content.parts[0] = }") # pyright: ignore[]
 
 
 if __name__ == "__main__":
