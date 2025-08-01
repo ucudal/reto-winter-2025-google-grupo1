06f6b65953781fc7f95dbbe38a458087785965a2
Cristian Rodriguez <cristianrodriguez@Juan-Martin-Hernandez.local>
2025-08-01 19:27:29 -0300

    remove duplicate main

--- src/main.py
+++ src/main.py
@@ -23,20 +23,6 @@ def main() -> None:
         response = chat.send_message(message)
         print(f"{(response.candidates or [])[0].content.parts[0] = }")  # pyright: ignore[]
 
-    client = genai.Client(api_key=env().google_cloud_api_key)
-
-    chat = client.chats.create(model="gemini-2.0-flash", config={"tools": []})
-
-    for message in [
-        "what the dog doin",
-        "whos a good boi",
-        "summarize what we have been talking about.",
-    ]:
-        print(f"{message = }")
-        response = chat.send_message(message)
-        print(f"{json.dumps(response.model_dump(mode="json")) = }")
-        print(f"{(response.candidates or [])[0].content = }")
-
 
 if __name__ == "__main__":
     main()
