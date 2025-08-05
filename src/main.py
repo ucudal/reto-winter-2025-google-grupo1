19fb368122491fd576441ab324a128e2eee4855d
Santiago Panozzo <97817883+SantiagoPanozzo@users.noreply.github.com>
2025-08-05 01:09:52 -0300

    #36 Added system prompt and basic Jinja template logic (#37)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 # Rabbit, you son of a lovely lady, this file is for goofing around and testing
 # stuff. Please, for all that is holy, do not bother me in prs with this file
 # or duplicated functionality because of the mess that this file is.
+import json
+from pydantic_ai.messages import ModelMessagesTypeAdapter
+from chat.memory import MEMORY_DIR
+from datetime import datetime
+
+from prompts.base_prompt import get_base_prompt
+from prompts.system_prompt import SystemPromptParams, get_system_prompt
+
 import asyncio
 from google import genai
 from google.cloud import bigquery
@@ -24,6 +32,11 @@ async def main() -> None:
     )
 
     print(f"{result = }")
+
+    system_prompt = get_system_prompt(
+        params=SystemPromptParams(date=datetime.now())
+    )
+    print(system_prompt)
 
 
 if __name__ == "__main__":
